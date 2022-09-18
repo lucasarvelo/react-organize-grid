7f513cc567ead6ec1115ffbd3b5f4a52d6da02fc
Lucas Arvelo <contact@lucasarvelo.com>
2022-09-18 21:59:10 +0100

    add resize handle

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { PointerEvent, useRef, useState } from "react";
 import {
   DndContext,
   closestCenter,
@@ -28,6 +28,8 @@ const App = () => {
     { id: 6, x: 1, y: 1, static: false, w: 1, h: 1 },
   ]);
 
+  const resizeHandleRef = useRef(null);
+
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -48,6 +50,27 @@ const App = () => {
     }
   };
 
+  const handleOnPointerDownResize = (
+    event: PointerEvent<HTMLDivElement>,
+    id: number
+  ) => {
+    if (event.currentTarget.parentElement?.parentElement)
+      console.log(
+        window.getComputedStyle(event.currentTarget.parentElement.parentElement)
+      );
+    console.log(id);
+  };
+
+  const handleOnPointerUpResize = (
+    event: PointerEvent<HTMLDivElement>,
+    id: number
+  ) => {};
+
+  const handleOnPointerMoveResize = (
+    event: PointerEvent<HTMLDivElement>,
+    id: number
+  ) => {};
+
   return (
     <div className={"grid gap-2 grid-cols-4 h-[540px]"}>
       <DndContext
@@ -65,7 +88,22 @@ const App = () => {
                 id={id}
                 className={`col-span-${itemLayout?.w} row-span-${itemLayout?.h} bg-slate-200`}
               >
-                <div className={"w-full h-full"}>{id}</div>
+                <div className={"w-full h-full relative"}>
+                  <div
+                    className={"w-2 h-4 absolute bg-black top-1/2 right-0"}
+                    onPointerDownCapture={(event) => {
+                      event.preventDefault();
+                      return handleOnPointerDownResize(event, id);
+                    }}
+                    onPointerUpCapture={(event) =>
+                      handleOnPointerUpResize(event, id)
+                    }
+                    onPointerMoveCapture={(event) =>
+                      handleOnPointerMoveResize(event, id)
+                    }
+                  ></div>
+                  {id}
+                </div>
               </SortableItem>
             );
           })}
